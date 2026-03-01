07949084c5a7cad53aa5fa9e541c3747f99357b8
¨Arruda¨ <joaopedroarruda323@gmail.com>
2026-03-01 20:22:53 +0000

    segundo push

---
+++ ex5EquacaoSegGrau.py
@@ -0,0 +1,24 @@
+import math
+
+## Inicio
+print('CALCULADORA DE RAIZ QUADRADA')
+
+a = float(input('Digite o valor de A: '))
+b = float(input('Digite o valor de B: '))
+c = float(input('Digite o valor de C: '))
+
+delta = (b*b) - 4*a*c
+
+## Saida
+if(delta > 0):
+    raiz1 = (-b + math.sqrt(delta)) / (2*a)
+    raiz2 = (-b - math.sqrt(delta)) / (2*a)
+    print ('\nRaiz 1 :', raiz1, '\n Raiz 2: ', raiz2)
+
+elif(delta == 0):
+    raiz1 = -b/(2*a)
+    print ('As duas raizes valem:', raiz1)
+
+elif(delta < 0):
+    print('Não existem raizes reais')
+
