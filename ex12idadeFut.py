6b56d6d7910cea6fac94fe3115b2dff86738406f
¨Arruda¨ <joaopedroarruda323@gmail.com>
2026-03-01 21:11:49 +0000

    vamoo

---
+++ ex12idadeFut.py
@@ -0,0 +1,9 @@
+## Inicio
+anoNasc = int(input('Digite o ano de seu nascimento:'))
+anoAtual = int(input('Digite o ano atual:'))
+
+idade = anoAtual - anoNasc
+
+idadeFutura = idade + 17
+
+print('Sua idade atual:', idade,'anos\nSua idade daqui 17 anos:', idadeFutura, 'anos')
