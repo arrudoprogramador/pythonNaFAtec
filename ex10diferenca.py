e2f4080707861973ddb2915743accb5a9a612217
¨Arruda¨ <joaopedroarruda323@gmail.com>
2026-03-01 21:00:36 +0000

    Posso fazer isso o dia todo

---
+++ ex10diferenca.py
@@ -0,0 +1,8 @@
+## Inicio
+numA = int(input('Digite o primeiro valor:'))
+numB = int(input('Digite o segundo valor:'))
+
+diferenca = numA - numB
+
+## Saida
+print('Resultado de', numA, '-', numB,':', diferenca)
