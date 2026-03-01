194f64798fb7163b09cd9b1f085eb2c2d1ab7d4e
¨Arruda¨ <joaopedroarruda323@gmail.com>
2026-03-01 20:54:30 +0000

    colevati lindao

---
+++ ex9somaQuadrado.py
@@ -0,0 +1,11 @@
+## Inicio
+numA = int(input('Digite o primeiro valor:'))
+numB = int(input('Digite o segundo valor:'))
+
+quaNumA = numA * numA
+quaNumB = numB * numB
+
+soma = quaNumA + quaNumB
+
+## Saida
+print('Soma dos quadrados:', soma)
