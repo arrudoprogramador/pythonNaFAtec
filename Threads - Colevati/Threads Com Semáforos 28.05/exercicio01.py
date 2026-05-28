c02b60d5cfbe8f18ec14689dcf3e35b090c4f907
arrudoprogramador <joaopedroarruda323@gmail.com>
2026-05-28 20:16:03 -0300

    feat: criação de arquivo Threads Com Semáforos pt2

---
+++ Threads - Colevati/Threads Com Semáforos 28.05/exercicio01.py
@@ -0,0 +1,18 @@
+import threading
+import random
+import time
+
+pistaNorte = threading.Semaphore()
+pistaSul = threading.Semaphore()
+
+def manobrar():
+    
+    pista.sleep(random(300,700))
+
+def taxiar():
+
+def decolagem():
+
+def afastamento():
+
+
